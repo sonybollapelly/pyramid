e74d3485802b654653549c18a37a70997186d2b8
sony <bollapellysony@gmail.com>
2024-12-02 14:59:10 +0530

    PyramidPattern.java with content bollapelly sony

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -3,12 +3,12 @@ public class PyramidPattern {
 		int rows=5;
 		for(int i=1;i<=rows;i++) {
 			for(int j=rows-i;j>0;j--) {
-				Sysyem.out.print(" ");
+				System.out.print(" ");
 			}
 			for(int k=1;k<=(2*i-1);k++) {
 				System.out.print("*");
 			}
-			Sysyem.out.println();
+			System.out.println();
 		}
 	}
 }
